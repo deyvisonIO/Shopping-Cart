5efd1215c6634b0285b5b7e3e7c21b5b623f17bd
Deyvison Iury <iurydeyvison@gmail.com>
2022-10-23 08:14:43 -0300

    Adding Shop List

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,11 +1,86 @@
+import { useEffect, useState } from "react"
+import { ShopCard } from "./ShopCard"
 
+const shopItems = [
+  {
+    img: '',
+    name: 'TEST',
+    price: 1000,
+  },
+  {
+    img: '',
+    name: '',
+    price: 0,
+  },
+  {
+    img: '',
+    name: '',
+    price: 0,
+  },
+  {
+    img: '',
+    name: '',
+    price: 0,
+  },
+  {
+    img: '',
+    name: '',
+    price: 0,
+  },
+  {
+    img: '',
+    name: '',
+    price: 0,
+  },
+  {
+    img: '',
+    name: '',
+    price: 0,
+  },
+  {
+    img: '',
+    name: '',
+    price: 0,
+  },
+  {
+    img: '',
+    name: '',
+    price: 0,
+  },
+  {
+    img: '',
+    name: '',
+    price: 0,
+  },
+  {
+    img: '',
+    name: '',
+    price: 0,
+  },
+  {
+    img: '',
+    name: '',
+    price: 0,
+  },
+]
 
 export function Shop() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    setItems(
+      shopItems.map(item => (
+        <ShopCard key={item.name} name={item.name} img={item.img} price={item.price} />
+      ))
+    )
+  })
+
   return (
     <div>
       <h1>Shop</h1>
-      <div>
-    
+      <div className="shopCards">
+        {items}
       </div>
     </div>
   )
